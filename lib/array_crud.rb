a9cf384f4d87300d9d717b3d9df7b6dc577e042d
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-11 02:15:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,6 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index= ["pink", "purple", "green", "orange"]
+  
   
 end
 
