3764f74799da2f5700ed1c62d1081d172d105762
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-11 02:08:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   remove_element_from_start_of_array= ["pink", "purple", "green", "orange"]
-  pink_array= remove_element_from_start_of_array.shift
+  wow_array= remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
